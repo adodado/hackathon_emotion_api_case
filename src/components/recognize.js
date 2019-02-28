36b9571c04cca7cc962c170e8915587bbe301f5e
mbjorck <Mgit clone https://mbjorck@bitbucket.org/panasso/panasso.git>
2019-02-28 18:28:55 +0100

    Update recognize.js

--- src/components/recognize.js
+++ src/components/recognize.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 
 import '../styles/register.css';
 
+
 class recognize extends Component {
     render() {
-
-    }
+       
+            setRef = webcam => {
+              this.webcam = webcam;
+            };
+           
+            capture = () => {
+              const imageSrc = this.webcam.getScreenshot();
+            };
+           
+            render() {
+              const videoConstraints = {
+                width: 1280,
+                height: 720,
+                facingMode: "user"
+              };
+           
+              return (
+                <div>
+                  <Webcam
+                    audio={false}
+                    height={350}
+                    ref={this.setRef}
+                    screenshotFormat="image/jpeg"
+                    width={350}
+                    videoConstraints={videoConstraints}
+                  />
+                  <button onClick={this.capture}>Capture photo</button>
+                </div>
+              );
+            }
+          }
+    
 }
 
 export default recognize;
